608c61cff954a2fba8bb7ec16f1b366683bf4cb4
konstantinuza <kostya2.7phoenix@gmail.com>
2025-01-09 11:11:44 +0300

    feat: completed 4/49

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,14 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const s1 = arr1.length;
+  const s2 = arr2.length;
+  const sizeArr = s1 >= s2 ? s1 : s2;
+  return Array.from(
+    { length: sizeArr },
+    (_, i) => (arr1[i] || 0) + (arr2[i] || 0)
+  );
 }
 
 /**
